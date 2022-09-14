f32129daa9d8a027e30126d896f8bfc1dc9ff0f5
abdelrahmanhassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2022-09-14 16:35:54 +0200

    adding README

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ This layout is built for android phones using some view groups like: <br>
 <li>Constraint Layout</li>
 </ul>
 </p>
+
+<p>The App supports the scrolling down for more details.</p>
+
+## Todo:
+
+<img src="./README_Assets/todo.png" />
+
+## Screenshots of project done:
+
+<img src="./README_Assets/project_done1.png" />
+
+<img src="./README_Assets/project_done2.png" />
---
+++ app/src/main/java/abdelrahman/magara/task1talabatlayout/MainActivity.kt
@@ -0,0 +1,22 @@
+package abdelrahman.magara.task1talabatlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    /*
+    Views and ViewGroup
+    ViewGroup:
+    ancient
+    1. LinearLayout
+    2. RelativeLayout
+    3. FrameLayout
+    4. ConstraintLayout
+     */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
