e4140fb09f3551e448d41739e693e6ec1398d5ba
abdelrahmanhassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2022-09-14 23:01:45 +0200

    Adding final styles

---
+++ app/src/main/java/abdelrahman/magara/task1talabatlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package abdelrahman.magara.task1talabatlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
